811c4dcf7b10ad6d2bbe3d6c3ee5b99c00745936
Zac Willmington <zac.willmington@gmail.com>
2018-06-22 17:08:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -138,26 +138,3 @@ class ApplicationController < Sinatra::Base
   end
 
 end
-
-helpers do
-
-    def self.current_user(session)
-            if session[:id]
-                @user = User.find(session[:id])
-            else
-                nil
-            end
-        end
-
-        def self.is_logged_in?(session)
-            if session[:id] &&  self.current_user(session).id == session[:id]
-                true
-            else
-                false
-            end
-        end
-
-        def self.is_params_empty?(params)
-            params['username'] == "" || params['email'] == "" || params['password'] == ""
-        end
-end
